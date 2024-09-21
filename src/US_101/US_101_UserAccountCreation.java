92800597fe64969d69838eb0c9ee8efb574a15ab
musalpsen <musalpsen@gmail.com>
2024-09-21 18:13:00 +0300

    deneme3

--- src/US_101/US_101_UserAccountCreation.java
+++ src/US_101/US_101_UserAccountCreation.java
@@ -3,7 +3,5 @@ package US_101;
 public class US_101_UserAccountCreation {
     public static void main(String[] args) {
         System.out.println("Merhaba");
-        System.out.println("Selam");
-        System.out.println("Selam2");
     }
 }
